26c6d20de2d190e985d390049b3a9b5630a73048
EASYGOING45 <1505674928@qq.com>
2023-04-09 14:58:13 +0800

    Request Message Class

--- message/message.h
+++ message/message.h
@@ -58,4 +58,86 @@ public:
 private:
 };
 
+// 继承自Message，对请求行的修改和获取，保存收到的首部选项
+class Request : public Message
+{
+public:
+    Request() : Message()
+    {
+    }
+    // 设置与返回请求行相关字段
+    void setRequestLine(const std::string &requestLine)
+    {
+        std::istringstream lineStream(requestLine);
+
+        // 获取请求方法
+        lineStream >> requestMethod;
+
+        // 获取请求资源
+        lineStream >> requestResource;
+
+        // 获取http版本
+        lineStream >> httpVersion;
+    }
+
+    // 对于Request报文，根据传入的一行首部字符串，向首部保存选项
+    void addHeaderOpt(const std::string &headLine)
+    {
+        static std::istringstream lineStream;
+        lineStream.str(headLine); // 以 istringstream的方式处理头部选项
+
+        std::string key, value; // 保存键值的临时量
+        lineStream >> key;      // 获取key
+        key.pop_back();         // 删除键中的冒号
+        lineStream.get();       // 删除冒号后的空格
+
+        // 读取空格之后所有的数据，遇到 \n 停止，所以 value 中还包含一个 \r
+        getline(lineStream, value);
+        value.pop_back(); // 删除其中的 \r
+
+        if (key == "Content-Length")
+        {
+            // 保存消息体的长度
+            contentLength = std::stoll(value);
+        }
+        else if (key == "Content-Type")
+        {
+            // 分离消息体类型。消息体类型可能是复杂的消息体，类似 Content-Type: multipart/form-data; boundary=---------------------------24436669372671144761803083960
+
+            // 先找出值中分号的位置
+            std::string::size_type semIndex = value.find(';');
+            // 根据分号查找的结果，保存类型的结果
+            if (semIndex != std::string::npos)
+            {
+                msgHeader[key] = value.substr(0, semIndex);
+                std::string::size_type eqIndex = value.find('=', semIndex);
+                key = value.substr(semIndex + 2, eqIndex - semIndex - 2);
+                msgHeader[key] = value.substr(eqIndex + 1);
+            }
+            else
+            {
+                msgHeader[key] = value;
+            }
+        }
+        else
+        {
+            msgHeader[key] = value;
+        }
+    }
+
+public:
+    // 数据成员字段
+    std::string recvMsg; // 收到但是还未处理的数据
+
+    std::string requestMethod;   // 请求消息的请求方法
+    std::string requestResource; // 请求的资源
+    std::string httpVersion;     // 请求的HTTP版本
+
+    long long contentLength = 0; // 记录消息体的长度
+    long long msgBodyRecvLen;    // 已经接收的消息体长度
+
+    std::string recvFileName;        // 如果客户端发送的是文件，记录文件的名字
+    FILEMSGBODYSTATUS fileMsgStatus; // 记录表示文件的消息体已经
+};
+
 #endif
