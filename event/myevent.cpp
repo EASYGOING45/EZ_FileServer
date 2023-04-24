83d9981429a44bc1aee65451ee8465f9d5796322
EASYGOING45 <1505674928@qq.com>
2023-04-24 09:26:28 +0800

    Part2

--- event/myevent.cpp
+++ event/myevent.cpp
@@ -59,5 +59,48 @@ void HandleRecv::process()
             modifyWaitFd(m_epollFd, m_clientFd, true, true, false);
             break;
         }
+
+        // 将收到的数据拼接到之前接收到的数据的后面，由于在处理文件时，里面可能有 \0 所以使用append将buf内的所有字符都保存到recvMsg中
+        requestStatus[m_clientFd].recvMsg.append(buf, recvLen);
+
+        // 边接收数据边处理
+        // 根据请求报文的状态执行操作，以下操作中，如果成功了，则解析请求报文的下个部分，如果某个部分还没有完全接收，会退出当前处理步骤，等再次收到数据后根据这次解析的状态继续处理
+
+        // 保存字符串查找结果，每次查找都可以用该变量暂存查找结果
+        std::string::size_type endIndex = 0;
+
+        // 如果是初始状态，获取请求行
+        if (requestStatus[m_clientFd].status == HANDLE_INIT)
+        {
+            endIndex = requestStatus[m_clientFd].recvMsg.find("\r\n"); // 查找请求行的结束边界
+            if (endIndex != std::string::npos)                         // npos是string::size_type的一个静态成员常量，值为-1，表示未找到子串
+            {
+                // 保存请求行
+                requestStatus[m_clientFd].setRequestLine(requestStatus[m_clientFd].recvMsg.substr(0, endIndex + 2)); // std::cout << requestStatus[m_clientFd].recvMsg.substr(0, endIndex + 2);
+                requestStatus[m_clientFd].recvMsg.erase(0, endIndex + 2);                                            // 删除收到的数据中的请求行
+                requestStatus[m_clientFd].status = HANDLE_HEAD;                                                      // 将状态设置为处理消息首部
+                std::cout << outHead("info") << "处理客户端" << m_clientFd << "的请求行完成" << std::endl;
+            }
+
+            // 如果没有找到 \r\n，表示数据还没有接收完成，会跳回上面继续接收数据
+        }
+
+        // 如果是处理首部的状态，逐行解析首部字段，直至遇到空行
+        if (requestStatus[m_clientFd].status == HANDLE_HEAD)
+        {
+            std::string curLine; // 用于暂存获取的一行数据
+            while (1)
+            {
+                endIndex = requestStatus[m_clientFd].recvMsg.find("\r\n"); // 获取一行的边界
+                if (endIndex == std::string::npos)
+                {
+                    // 如果没有找到边界，表示后面的数据还没有接收完整，退出循环，等待下次接收后处理
+                    break;
+                }
+
+                curLine = requestStatus[m_clientFd].recvMsg.substr(0, endIndex + 2); // 将该行的内容取出
+                requestStatus[m_clientFd].recvMsg.erase(0, endIndex + 2);            // 删除收到的数据中的该行数据
+            }
+        }
     }
 }
